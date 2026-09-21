c0744cd4eed59a6e94e1ff55459ca505c0d434ee
Fatade Adebola Victor <158852298+fatadde@users.noreply.github.com>
2026-09-21 21:21:08 +0100

    publish

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import '../src/style/home.css'
 import '../src/style/about.css'
+import '../src/style/responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
